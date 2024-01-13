7fbd213f5f9db08e12002deddafc8f28b8c6c1c1
aneelaehsan1 <117081199+aneelaehsan1@users.noreply.github.com>
2024-01-13 21:55:32 +0500

    update

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const app = express()
-require('dotenv').config()
-const port = process.env.SERVER_PORT
+const cors = require('cors')
+const connectDB = require('./utilis/db')
 const userRoute = require('./routers/users')
-var cors = require('cors') 
+const authRoute = require('./routers/auth')
+const postRoute = require('./routers/posts')
+const commentRoute = require('./routers/comments')
 
-//MIDDLEWARE
+
+
+// middleware
+
 app.use(cors())
 app.use(express.json())
+app.use('/api',authRoute)
+app.use('/api/posts',postRoute)
 app.use('/api',userRoute)
+app.use('/api/comments',commentRoute)
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    connectDB()
+    console.log("app is running on port "+ process.env.PORT)
 })
+
